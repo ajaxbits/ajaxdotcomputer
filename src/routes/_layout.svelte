54967a2ff7997387d1932f97e9d2d103529f9269
Alex Jackson <45179933+beattheprose@users.noreply.github.com>
2020-06-12 20:11:45 -0500

    Get the initial changes in

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -14,7 +14,7 @@
   }
 </style>
 
-<Nav {segment} />
+<!-- <Nav {segment} /> -->
 
 <main>
   <slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,24 +1,73 @@
 <script>
-  import Image from "svelte-image";
+
 </script>
 
 <style>
+  h1 {
+    font-size: 2.8em;
+    font-weight: normal;
+    font-style: italic;
+    margin: 0 0 0.5em 0;
+  }
 
+  h2 {
+    margin-top: 2em;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
 </style>
 
 <svelte:head>
   <title>ajaxdotcomputer</title>
 </svelte:head>
 
-<Image src="headshot.jpg" />
+<h1>
+  hey,
+  <br />
+  I'm Alex Jackson
+</h1>
 
-<h1>hey</h1>
+<p>I'm a junior web developer centered in Chicago.</p>
 
-<h1>Welcome to ajaxdotcomputer</h1>
+<a href="https://github.com/beattheprose">
+  <p>Check out more at my GitHub</p>
+</a>
 
-<h1>I'm Alex</h1>
+<h2 id="#portfolio">Check my stuff out</h2>
+<a href="https://gracebobber.netlify.app">
+  Pure HTML and CSS Website for my partner
+</a>
 
-<p>
-  I'm a junior web developer centered in Chicago. Find my github
-  <a href="www.google.com">here!</a>
-</p>
+<h2 id="#skills">Skills</h2>
+<ul>
+  <li>HTML</li>
+  <li>CSS</li>
+  <li>git & GitHub</li>
+  <li>Adobe Photoshop</li>
+  <li>Adobe Illustrator</li>
+  <li>Adobe Indesign</li>
+  <li>Linux</li>
+</ul>
+
+<h3>Things I'm Learning</h3>
+<ul>
+  <li>Javascript</li>
+  <li>
+    Svelte/Sapper
+    <a href="https://github.com/beattheprose/ajaxdotcomputer">
+      <em>(this website!)</em>
+    </a>
+  </li>
+  <li>React</li>
+</ul>
+<h3>Next Steps</h3>
+<ul>
+  <li>Tests</li>
+  <li>Open source contributions</li>
+  <li>Typescript</li>
+  <li>Nix-env</li>
+</ul>
