d9d48ddbed51ba81e140a39cae019686cf4e46ac
beattheprose <45179933+beattheprose@users.noreply.github.com>
2020-09-19 00:34:42 +0000

    add code notes

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,6 +58,11 @@ export default function IndexPage() {
                 </Link></li>
               <li><Link style={{
                 color: `#efefef`,
+              }} to="https://notes.ajax.computer">
+                My public notes (Gatsby + React)
+                </Link></li>
+              <li><Link style={{
+                color: `#efefef`,
               }}>
                 This website (Gatsby + React)
                 </Link></li>
