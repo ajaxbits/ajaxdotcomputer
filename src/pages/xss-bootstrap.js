70287e1be745565992ae5fdba2f7ba73fdd5819a
Alex Jackson <jaxsalex@pm.me>
2021-01-03 14:21:23 -0600

    add xss page

---
+++ src/pages/xss-bootstrap.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+  return (
+          <!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Wrecked</title>
+</head>
+<body>
+        <script type="text/javascript">
+                document.cookie = "lmao hackedddd";
+                document.write(document.cookie);
+        </script>
+</script>
+</body>
+</html>
+
+  )
+}
+
